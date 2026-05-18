b7ac31cf34cd7c02da921ef600ca4d30130710f6
ducatione <edujiaoyu@pm.me>
2026-05-18 12:56:44 +0100

    Initial commit

---
+++ src/main/java/CA_2/CA_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package CA_2;
+
+/**
+ *
+ * @author kkter
+ */
+public class CA_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
